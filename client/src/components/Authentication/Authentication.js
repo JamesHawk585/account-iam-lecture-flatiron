9b395261576e86d24d5a2d7fa1229ce64b28e829
James Hawk <jph94880@gmail.com>
2024-03-21 20:48:51 -0500

    Adds ternary to fetch request in handleSubmit() in Authentication.js

--- client/src/components/Authentication/Authentication.js
+++ client/src/components/Authentication/Authentication.js
@@ -12,27 +12,16 @@ const Authentication = ({ updateUser }) => {
 
   const handleSignUpClick = () => setSignUp((signUp) => !signUp);
 
-  /*
-    - Finish building the authentication controlled for to handle the:
-        - value
-        - onchange
-        - onsubmit of the form
-    - on submit create a POST. 
-        - There is a button that toggles the component between login and sign up.
-        - if signUp is true use the path '/users' else use '/login' (we will be writing login soon)
-        - Complete the post and test our '/users' route 
-    - On a successful POST add the user to state (updateUser is passed down from app through props) and redirect to the Home page.
-    - return to server/app.py to build the next route
-*/
+
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const config = {
       method: "POST",
       headers: { "content-type": "application/json" },
-      body: JSON.stringify(userData),
+      body: JSON.stringify(signUp ? userData : userData.name),
     };
-    fetch("/users", config)
+    fetch(signUp ? "/users" : "/login", config)
       .then((r) => r.json())
       .then((user) => {
         updateUser(user);

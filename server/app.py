1c3ad4ccc3badc7fab2f697b1c88206e1e494677
James Hawk <jph94880@gmail.com>
2024-03-25 15:50:11 -0500

    Adds check_for_missing_values() function to app.py

--- server/app.py
+++ server/app.py
@@ -5,11 +5,22 @@ from models import User, Production, CrewMember
 
 from config import app, api, db
 
+def check_for_missing_values(data):
+    import ipdb; ipdb.set_trace()
+    errors = []
+    for key, value in data.items():
+        if not value:
+            errors.append(f"{key} is required")
+    return errors
 
 # Using flask-restful
 class Users(Resource):
     def post(self):
         data = request.get_json()
+        errors = check_for_missing_values(data)
+
+
+
         user = User(
             name=data["name"], 
             email=data["email"]
